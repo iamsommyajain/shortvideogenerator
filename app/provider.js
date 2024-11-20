ac6f6904fefe1031c6fc0d2af7a5d1432e2eee8c
Sommya Jain <sommyajain2005@gmail.com>
2024-11-21 02:46:18 +0530

    second commit

--- app/provider.js
+++ app/provider.js
@@ -1,7 +1,26 @@
-import React from 'react'
-
-
+"use client"
+import React, { useEffect } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { db } from '../configs/db.js'; // Ensure db is properly imported
+import { Users } from '../configs/schema.js'; // Ensure your Users schema is correctly imported
+import { eq } from 'drizzle-orm';
 function Provider({children}) {
+    const {user}=useUser();
+    useEffect(()=>{
+        user&&isNewUser();
+    },[user]);
+    const isNewUser=async()=>{
+        const result=await db.select().from(Users)
+        .where(eq(Users.email,user?.primaryemailAddress?.emailAddress));
+        console.log(result);
+        if (!result[0]) {
+            await db.insert(Users).values({
+                name:user.fullName,
+                email:user?.primaryemailAddress?.emailAddress,
+                imageUrl:user?.imageUrl
+            })
+        }
+    }
     return (
         <div>
             {children}
